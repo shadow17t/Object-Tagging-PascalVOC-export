52517d715e5e8fa431448fdf52be48e95ee779ed
shadow17t <shadow.17t@gmail.com>
2021-07-29 01:13:25 +0800

    perbaikan

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import random
 from numpy import append, floor
 import pandas as pd
 import xml.etree.ElementTree as ET
@@ -10,29 +11,22 @@ import xml.etree.ElementTree as ET
 # 3rd col= label
 # 4th col= bounding box(2 titik: xmin, ymin,, , xmax, ymax,, / 4titik: xmin, ymin, xmax, ymin, xmax, ymax, xmin, ymax)
 
-# root = r"/content/Object-Tagging-PascalVOC-export" #root directory path
-# root_path = root +'/Annotations/' #input folder
-img_path=os.path.join(os.getcwd(), 'Object-Tagging-PascalVOC-export/Annotations/')
+img_dir=os.path.join(os.getcwd(), 'Object-Tagging-PascalVOC-export/JPEGImages/')
 
-def xml_to_csv(path):
-    xml_list = []
-    for xml_file in glob.glob(path + '/*.xml'):
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
-        for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
-            xml_list.append(value)
-    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
-    xml_df = pd.DataFrame(xml_list, columns=column_name)
-    return xml_df
+image_paths = os.listdir(img_dir)
+random.shuffle(image_paths)
+
+train_size=0.8
+validation_size=0.1
+test_size=0.1
+n_images=len(image_paths)
+
+train_data = image_paths[:int((n_images+1)*train_size)]
+val_data = image_paths[int((n_images+1)*train_size):(int((n_images+1)*train_size)+int((n_images+1)*validation_size))]
+test_data = image_paths[(int((n_images+1)*train_size)+int((n_images+1)*validation_size)):]
+print(len(image_paths))
+print(len(train_data),len(val_data),len(val_data))
+
 
 def xml_to_csv_train(path):
     xml_list = []
@@ -57,14 +51,21 @@ def xml_to_csv_train(path):
             if ymaxrel > 1:
               ymaxrel=floor(ymaxrel)
 
-            tipe='TRAIN'
-            image_path=root.find('filename').text
+            image_filename=root.find('filename').text
+            if image_filename in train_data:
+                data_type='TRAIN'
+            elif image_filename in val_data:
+                data_type='VALIDATION'
+            elif image_filename in test_data:
+                data_type='TEST'
+            
             label=member[0].text
-            bbox=str(xminrel)+','+str(yminrel)+',,,'+str(xmaxrel)+','+str(ymaxrel)+',,'
-            value = (tipe,
-                    img_path+(image_path),
+            # bbox=str(xminrel)+','+str(yminrel)+',,,'+str(xmaxrel)+','+str(ymaxrel)+',,'
+            value = (data_type,
+                    img_dir+(image_filename),
                     label,
-                    bbox
+                    # bbox
+                    xminrel,yminrel,'','',xmaxrel,ymaxrel,'',''
                     )
             xml_list.append(value)
     xml_df = pd.DataFrame(xml_list)
